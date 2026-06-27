2eef06dda27854ec92760034514442dc30b33a58
Aravind Murugesan <aravind.murugesan@payground.com>
2026-06-27 06:50:34 -0400

    Fix indentation in check_password

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ def check_password():
         st.text_input("Password", type="password", key="pwd_input")
         if st.button("Login"):
             try:
-            app_password = st.secrets["APP_PASSWORD"]
-        except Exception:
-            app_password = os.environ.get("APP_PASSWORD", "")
-        if st.session_state.pwd_input == app_password:
+                app_password = st.secrets["APP_PASSWORD"]
+            except Exception:
+                app_password = os.environ.get("APP_PASSWORD", "")
+            if st.session_state.pwd_input == app_password:
                 st.session_state.authenticated = True
                 st.rerun()
             else:
